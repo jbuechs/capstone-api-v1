ff8663d0dc4ac973311a3f1bb2f2447b81632e84
Jennie Buechner <janina.buechner@gmail.com>
2016-03-03 15:06:51 -0800

    create get adies/:id route

--- routes/adies.js
+++ routes/adies.js
@@ -1,14 +1,32 @@
+/* jshint esnext:true */
+
 var express = require('express');
 var router = express.Router();
 var db = require('../models');
 // var pry = require('pryjs');
 
 // GET adies path
-router.get('/', function(req, res, next) {
-	// eval(pry.it);
+router.get('/', function(req, res) {
 	db.adie.findAll()
-		.then(function(adies) {
-			res.send({data: adies});
+		.then(adies => res.send({data:adies}));
+		// add catch for errors?
+});
+
+// GET adies/:id path
+router.get('/:id', function(req, res) {
+	db.adie.findById(req.params.id)
+		.then(function(adie) {
+			if (adie === null) {
+				res.statusCode = 404;
+      	return res.json({ errors: ['Adie not found']});
+			}
+			res.send({data:adie});
+		})
+		// need to write a mock that tests this
+		.catch(function(err) {
+			console.error(err);
+			res.statusCode = 500;
+			return res.json({ errors: ['Could not retrieve adie'] });
 		});
 });
 
