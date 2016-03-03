2187a42969081210325281e3cf844f1504af4c8c
Jennie Buechner <janina.buechner@gmail.com>
2016-03-03 09:34:45 -0800

    Adies model has companyId not internshipId

--- models/adie.js
+++ models/adie.js
@@ -39,7 +39,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        Adie.belongsTo(models.Company, { as: 'internship'});
+        Adie.belongsTo(models.Company);
       }
     }
   });
