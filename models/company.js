29968cffebf87d07c3f608afab76a95aa818c71a
Jennie Buechner <janina.buechner@gmail.com>
2016-03-07 13:49:55 -0800

    bugfix change sequelize to datatypes

--- models/company.js
+++ models/company.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize, DataTypes) {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: DataTypes.INTEGER
     },
     name: {
       type: DataTypes.STRING,
