24ca91d0f791989128edff3805d6b67c856e7b5b
Jennie Buechner <janina.buechner@gmail.com>
2016-03-07 12:03:06 -0800

    add delete method

--- routes/adies.js
+++ routes/adies.js
@@ -1,4 +1,5 @@
 /* jshint esnext:true */
+var pry = require('pryjs');
 
 var express = require('express');
 var router = express.Router();
@@ -21,10 +22,25 @@ router.get('/:id', lookupAdie, function(req, res) {
 router.post('/', function(req, res) { });
 
 // PATCH to adies/:id path
+router.patch('/:id', lookupAdie, function(req, res){
 
+});
 
 // DELETE to adies/:id path
-
+router.delete('/:id', lookupAdie, function(req, res){
+	// eval(pry.it);
+	db.adie.destroy({
+		where: { id : req.params.id }
+	})
+	.then(function() {
+		return res.json({ message: ['Adie successfully deleted']});
+	})
+	.catch(function() {
+		console.error(err);
+		res.statusCode = 500;
+		return res.json({ errors: ['Could not delete adie'] });
+	});
+});
 
 // Lookup Adie by id
 function lookupAdie(req, res, next) {
