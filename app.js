25d3ed24b981deabb093941d1c9db5126a0de899
Jennie Buechner <janina.buechner@gmail.com>
2016-03-10 11:50:21 -0800

    add some comments to organize

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+// Dependencies
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Routes
 var routes = require('./routes/index');
 var adies = require('./routes/adies');
 var employees = require('./routes/employees');
