a258a0ef7d02cc5299b207e364362fca8210d2b9
Jennie Buechner <janina.buechner@gmail.com>
2016-03-11 16:46:20 -0800

    add authentication check to get/:id

--- routes/companies_employees.js
+++ routes/companies_employees.js
@@ -4,29 +4,39 @@ var router = express.Router();
 var db = require('../models');
 // var pry = require('pryjs');
 
-// GET companies_employees/:id path
-// Add authorization
-router.get('/:id([0-9]+)', lookupCompany, function(req, res) {
-	var response = { data : {
-		company: req.data
-	}};
-	db.sequelize.query('SELECT * FROM employees WHERE "companyId" = ?', { replacements: [req.params.id], type: db.sequelize.QueryTypes.SELECT})
-  .then(function(employees) {
-    response.data.employees = employees;
-    return res.json(response);
-  });
+// TODO: See if I can dry the dotenv and jwt code up
+var dotenv = require('dotenv');
+var jwt = require('express-jwt');
+dotenv.load();
+var jwtCheck = jwt({
+  secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'),
+  audience: process.env.AUTH0_CLIENT_ID,
 });
 
-// Lookup company by id
-function lookupCompany(req, res, next) {
-	db.company.findById(req.params.id)
+// GET companies_employees/:id path
+router.get('/:id([0-9]+)', function(req, res) {
+	jwtCheck(req, res, function() {
+		// check for authentication through req.user
+		db.company.findById(req.params.id, {attributes: {exclude: ['createdAt', 'updatedAt'] }})
 		.then(function(company) {
 			if (company === null) {
 				res.statusCode = 404;
       	return res.json({ errors: ['Company not found']});
 			}
-			req.data = company;
-			next();
+			var response = { data : {
+				company: company
+			}};
+			if (req.user) {
+				// user is authenticated, so search for employees
+				db.sequelize.query('SELECT * FROM employees WHERE "companyId" = ?', { replacements: [req.params.id], type: db.sequelize.QueryTypes.SELECT})
+				  .then(function(employees) {
+				    response.data.employees = employees;
+				    return res.json(response);
+				  });
+			} else {
+				// user is not authenticated, so no employees & just return the company
+				return res.json(response);
+			}
 		})
 		// need to write a mock that tests this
 		.catch(function(err) {
@@ -34,6 +44,8 @@ function lookupCompany(req, res, next) {
 			res.statusCode = 500;
 			return res.json({ errors: ['Could not retrieve company'] });
 		});
-}
+	});
+	
+});
 
 module.exports = router;
